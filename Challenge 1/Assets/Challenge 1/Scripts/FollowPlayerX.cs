0eba177a9e289a7e694dd5bdd70a331e1239f85c
Adriana Rivas <10940794@my.uvu.edu>
2021-03-15 00:33:33 -0600

    Completed Challenge 1

---
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Offset camera position by plane position
+        offset = transform.position - plane.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update camera position
+        transform.position = plane.transform.position + offset;
+    }
+}
