8b17612466d9f5506e836db024db3a7e88fb21db
Adriana Rivas <10940794@my.uvu.edu>
2021-03-22 23:15:18 -0600

    Up to Lesson 2.3

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
     public float hInput;
+    private float xRange = 24;
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     { 
+
     }
+
     // Update is called once per frame
     void Update()
     {
         hInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
-        if(transform.position.x < -10)
+
+        if(transform.position.x < -xRange)
         {
-         transform.position = new Vector3(-10, transform.position.y, transform.position.z); 
-        }   
+         transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
+        }
+        if(transform.position.x > xRange) 
+        {
+         transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch the projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }    
+
     }
 }
