ca32d17936b90683eed7dafacb88525186a19289
Adriana Rivas <10940794@my.uvu.edu>
2021-04-02 01:08:37 -0600

    Completed Challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        // Destroy this object that its script is attached to
+        Destroy(gameObject);
+        
+        // Destroy other object hits a trigger
+        Destroy(other.gameObject);
+    }
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnDelay = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dog spawns once per second
+        spawnDelay += Time.deltaTime;
+        if (spawnDelay > 1.0f)
+        {
+            spawnDelay = 0.0f;
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
